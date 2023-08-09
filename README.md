5ba6bda811f43f4b94038ae4188e37cec13a6577
Azeez <azeezade04@gmail.com>
2023-08-09 11:03:57 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+HTML to PDF converter
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import puppeteer, { Browser, PDFOptions, Page } from 'puppeteer';
+
+export const generatePdf = async (pdfOptions: PDFOptions, html: string) => {
+  // Create a browser instance
+  const browser: Browser = await puppeteer.launch({
+    headless: true,
+    args: [`--window-size=827,992`],
+  });
+
+  // Create a new page
+  const page: Page = (await browser.pages())[0];
+
+  // render HTML file on browser page
+  await page.setContent(html, {
+    waitUntil: ['domcontentloaded', 'networkidle0'],
+  });
+
+  // To reflect CSS used for screens instead of print
+  await page.emulateMediaType('screen');
+
+  // Downlaod the PDF
+  const buffer = await page.pdf(pdfOptions);
+
+  // Close the browser instance
+  await browser.close();
+  console.log(buffer);
+  return buffer;
+};
